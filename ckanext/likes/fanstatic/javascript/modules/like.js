3d309190b59e1f34eb2c39f37157bd869aaa7f2d
Pavle Jonoski <pavle.jonoski@keitaro.com>
2018-07-19 11:18:49 +0200

    Fixed likes snippet. Fixed the formatting a bit in the like.js script.

--- ckanext/likes/fanstatic/javascript/modules/like.js
+++ ckanext/likes/fanstatic/javascript/modules/like.js
@@ -12,42 +12,42 @@
  *
  */
 this.ckan.module('like', function($) {
-	return {
-		/* options object can be extended using data-module-* attributes */
-		options : {
-			action: null,
-			type: null,
-			id: null,
-			loading: false
-		},
+    return {
+        /* options object can be extended using data-module-* attributes */
+        options : {
+            action: null,
+            type: null,
+            id: null,
+            loading: false
+        },
 
-		/* Initialises the module setting up elements and event listeners.
-		 *
-		 * Returns nothing.
-		 */
-		initialize: function () {
-			$.proxyAll(this, /_on/); 
+        /* Initialises the module setting up elements and event listeners.
+         *
+         * Returns nothing.
+         */
+        initialize: function () {
+            $.proxyAll(this, /_on/); 
             this.el.on('click', this._onClick);
-		},
+        },
 
-		/* Handles the clicking of the like button
-		 *
-		 * event - An event object.
-		 *
-		 * Returns nothing.
-		 */
-		_onClick: function(event) {
-			var options = this.options;
-			if (
-				options.action
-				&& options.type
-				&& options.id
-				&& !options.loading
-			) {
-				event.preventDefault();
+        /* Handles the clicking of the like button
+         *
+         * event - An event object.
+         *
+         * Returns nothing.
+         */
+        _onClick: function(event) {
+            var options = this.options;
+            if (
+                options.action
+                && options.type
+                && options.id
+                && !options.loading
+            ) {
+                event.preventDefault();
                 var client = this.sandbox.client;
-				var path = 'likes_' + options.action + '_' + options.type;
-				options.loading = true;
+                var path = 'likes_' + options.action + '_' + options.type;
+                options.loading = true;
                 this.el.addClass('disabled');
                 var data = null
                 if (options.type == 'dataset'){
@@ -55,34 +55,34 @@ this.ckan.module('like', function($) {
                 } else if (options.type == 'resource') {
                     data = { resource_id : options.id }
                 }else if (options.type == 'request') {
-					data = { id : options.id }
-				}else {
-					throw Exception("Unknown type: " + options.type)
-				}
-				client.call('POST', path, data, this._onClickLoaded);
-			}
-		},
+                    data = { id : options.id }
+                }else {
+                    throw Exception("Unknown type: " + options.type)
+                }
+                client.call('POST', path, data, this._onClickLoaded);
+            }
+        },
 
-		/* Fired after the call to the API to either like or dislike
-		 *
-		 * json - The return json from the like / dislike API call
-		 *
-		 * Returns nothing.
-		 */
-		_onClickLoaded: function(json) {
+        /* Fired after the call to the API to either like or dislike
+         *
+         * json - The return json from the like / dislike API call
+         *
+         * Returns nothing.
+         */
+        _onClickLoaded: function(json) {
             var options = this.options;
             var sandbox = this.sandbox;
-			var oldAction = options.action;
-			options.loading = false;
-			this.el.removeClass('disabled');
-			if (options.action == 'like') {
-				options.action = 'dislike';
-				this.el.html('<i class="fa fa-times-circle"></i> ' + this._('Dislike')).removeClass('btn-success').addClass('btn-danger');
-			} else {
-				options.action = 'like';
-				this.el.html('<i class="fa fa-plus-circle"></i> ' + this._('Like')).removeClass('btn-danger').addClass('btn-success');
+            var oldAction = options.action;
+            options.loading = false;
+            this.el.removeClass('disabled');
+            if (options.action == 'like') {
+                options.action = 'dislike';
+                this.el.html('<i class="fa fa-times-circle"></i> ' + this._('Dislike')).removeClass('btn-success').addClass('btn-danger');
+            } else {
+                options.action = 'like';
+                this.el.html('<i class="fa fa-plus-circle"></i> ' + this._('Like')).removeClass('btn-danger').addClass('btn-success');
             }
             sandbox.publish('like-' + oldAction + '-' + options.id);
-		}
-	};
+        }
+    };
 });
