92ffce8a8f36917ae97751e043c8f6a394d093a1
miloshIra <milosh.aritonski@keitaro.com>
2023-05-29 14:56:17 +0200

    moved setup() from plugin config to utils_db

--- ckanext/likes/utils_db.py
+++ ckanext/likes/utils_db.py
@@ -6,6 +6,8 @@ log = __import__('logging').getLogger(__name__)
 Base = declarative_base()
 
 def init_tables(engine):
+    from .model import setup
+    setup()
     Base.metadata.create_all(engine)
 
     
